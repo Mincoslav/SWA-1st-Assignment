414d4d3c386bdc29976b0af270b0142e18c3770a
Marwan Summakieh <42711091+MarwanSummakieh@users.noreply.github.com>
2021-09-14 13:04:19 +0200

    DataIntervals implemented (I think)

--- exercise1.1.js
+++ exercise1.1.js
@@ -23,4 +23,21 @@ function DataType(type, unit) {
         getType,
         getUnit
     }
-}
+};
+
+function DataInterval(from, to, date){
+    const _from = from
+    const _to = to
+    const _date = date
+
+    const getFrom = ()=> _from
+    const getTo = ()=> _to
+    const contains = function(_date){
+        if(_date>_from && _date<_to){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+} 
