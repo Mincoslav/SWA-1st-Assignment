2b6a24430f98b75140870f7c10f390700d662714
Michal <mincoslav@gmail.com>
2021-09-16 15:51:03 +0200

    Added temperature, wind, precipitation factories.

--- exercise1.1.js
+++ exercise1.1.js
@@ -1,9 +1,12 @@
-function Event(state, time, place) {
-    const _time = time
-    const _place = place
+function eventNature(state) {
 
-    const getTime = () => _time
-    const getPlace = () => _place
+    function getTime() {
+        return state.time
+    } 
+
+    function getPlace() {
+        return state.place
+    } 
 
     return {
         getTime,
@@ -12,12 +15,14 @@ function Event(state, time, place) {
 };
 
 
-function DataType(state, type, unit) {
-    const _type = type
-    const _unit = unit
+function dataType(state) {
 
-    const getType = () => _type
-    const getUnit = () => _unit
+    function getType() {
+        return state.type
+    } 
+    function getUnit() {
+        return state.unit
+    } 
 
     return {
         getType,
@@ -25,18 +30,20 @@ function DataType(state, type, unit) {
     }
 };
 
-function DateInterval(from, to, date){
-    const _from = from
-    const _to = to
-    const _date = date
+function dateInterval(state){
+    
+    function getFrom() {
+        return state.from
+    } 
+    function getTo() {
+        return state.to
+    }
 
-    const getFrom = ()=> _from
-    const getTo = ()=> _to
-    const contains = function(){
-        if(_date>_from && _date<_to){
+    function contains(date) {
+        if(date>getFrom() && date<getTo()){
             return true
         }
-        else{
+        else {
             return false
         }
     }
@@ -48,12 +55,213 @@ function DateInterval(from, to, date){
     }
 } 
 
-const fromTester = new Date('December 17, 1995 03:24:00')
-const toTester = new Date('December 17, 2021 05:00:00')
-const dateTester = new Date()
-const intervalTester = new DataInterval(fromTester, toTester, dateTester)
 
-console.log(`${intervalTester}`)
+function weatherData(state) {
+    
+    const event = eventNature(state)
+    const type = dataType(state)
+
+    function getValue() {
+        return state.value
+    }
+
+    return {
+        ...event, ...type, getValue
+    }
+}
+
+// const state = {
+//     time: Date(),
+//     place: 'Horsens',
+//     value: '123'
+// }
+// const data = weatherData(state)
+// console.log(data.getTime())
+
+function temperature(time, place, type, unit, value) {
+    const state = {time, place, type, unit, value}
+    const weatherDataState = weatherData(state)
+
+    function convertToF() {
+        if (state.unit === 'C') {
+            state.unit = 'F'
+            state.value = state.value * 1.8 + 32
+        }
+    }
+
+    function convertToC() {
+        if (state.unit === 'F') {
+            state.unit = 'C'
+            state.value = (state.value - 32) / 1.8
+        }
+    }
+
+    function toString() {
+        return state
+    }
+
+    return {
+        ...weatherDataState, convertToC, convertToF, toString
+    }
+}
+
+// TESTER CODE
+// const data = temperature(time=Date(), place = 'Horsens', type='Temperature', unit='C', value='123' )
+// console.log(data.toString())
+// console.log(data.getUnit())
+// console.log(data.getValue())
+// data.convertToF()
+// console.log(data.getUnit())
+// console.log(data.getValue())
+// console.log(data.getPlace())
+
+function precipitation(time, place, type, unit, value) {
+    const state = {time, place, type, unit, value}
+    const weatherDataState = weatherData(state)
+
+    function getPrecipitationType() {
+        return state.type
+    }
+
+    function convertToInches() {
+        if (state.unit === 'MM') {
+            state.unit = 'Inch'
+            state.value = state.value / 25.4
+        }
+    }
+
+    function convertToMM() {
+        if (state.unit === 'Inch') {
+            state.unit = 'MM'
+            state.value = state.value * 25.4
+        }
+    }
+
+    function toString() {
+        return state
+    }
+
+    return {
+        ...weatherDataState, getPrecipitationType, convertToInches, convertToMM, toString
+    }
+}
+
+// TESTER CODE
+const data = precipitation(time=Date(), place='Horsens', type='Precipitation', unit='MM', value=30 )
+console.log(data.toString())
+console.log(data.getUnit())
+console.log(data.getValue())
+data.convertToInches()
+console.log(data.getUnit())
+console.log(data.getValue())
+console.log(data.getPlace())
 
 
-console.log(`DATE CONTAINS: ${intervalTester.contains()}`)
+function wind(direction, time, place, type, unit, value) {
+    const state = {direction, time, place, type, unit, value}
+    const weatherDataState = weatherData(state)
+
+    function getDirection() {
+        return state.direction
+    }
+
+    function convertToMPH() {
+        if (state.unit === 'M/S') {
+            state.unit = 'MPH'
+            state.value = state.value * 2.237
+        }
+    }
+
+    function convertToMperS() {
+        if (state.unit === 'MPH') {
+            state.unit = 'M/S'
+            state.value = state.value / 2.237
+        }
+    }
+
+    function toString() {
+        return state
+    }
+
+    return {
+        ...weatherDataState, getDirection, convertToMPH, convertToMperS, toString
+    }
+}
+// TESTER CODE
+// const data = wind(direction='West', time=Date(), place= 'Horsens', type='Wind', unit='M/S', value=3 )
+// console.log(data.toString())
+// console.log(data.getUnit())
+// console.log(data.getValue())
+// data.convertToMPH()
+// console.log(data.getUnit())
+// console.log(data.getValue())
+// console.log(data.getPlace())
+
+
+// IS EMPTY???
+// function cloudCoverage(cloudPercentage, time, place, type, unit, value) {
+//     const state = {cloudPercentage, time, place, type, unit, value}
+//     const weatherDataState = weatherData(state)
+
+//     function getCloudPercentage() {
+//         return state.cloudPercentage
+//     }
+
+//     return {
+//         ...weatherDataState, getCloudPercentage
+//     }
+// }
+
+
+
+
+
+// function WeatherHistory() {
+    
+// }
+
+
+
+// function weatherPrediction(weatherPredictionState) {
+//     const _data = weatherData()
+//     const state = {weatherPredictionState}
+//     const event = eventNature(state)
+//     const type = dataType(state)
+
+//     function matches(data) {
+//         return data.time === _data.getTime() 
+//         && data.place === _data.getPlace()
+//         && data.type === _data.getType()
+//         && data.unit === _data.getUnit()
+//         && data.value === _data.getValue()
+//     }
+
+//     function getMin() {
+//         return 0
+//     }
+
+//     function getMax() {
+//         return 999
+//     }
+
+//     return {
+//         ...event, ...type, matches, getMax, getMin
+//     }
+// }
+
+// function temperaturePrediction() {
+
+// }
+
+// // const fromTester = new Date('December 17, 1995 03:24:00')
+// // const toTester = new Date('December 17, 2021 05:00:00')
+// // const dateTester = new Date()
+// // const state = {
+// //     fromTester, toTester, dateTester
+// // }
+// // const intervalTester = new DateInterval(state)
+
+// // console.log(`${intervalTester.getFrom()}`)
+
+
+// // console.log(`DATE CONTAINS: ${intervalTester.contains(state.dateTester)}`)
