1db7f4e64d03c628073ea205ea8d43a6440cd804
Marwan Summakieh <42711091+MarwanSummakieh@users.noreply.github.com>
2021-09-20 16:26:48 +0200

    trying to fix the filter

--- exercise1.2.js
+++ exercise1.2.js
@@ -1,169 +1,269 @@
 /*###########Event_DataType class############*/
 
-let Event_DataType = function(time, place, type, unit) {
-  this.place = place;
-  this.time = time; 
-  this.type = type;
-  this.unit = unit;  
-}
-Event_DataType.prototype.getTime = function () {
-  return this.time  
-}
-Event_DataType.prototype.getPlace = function () {
-  return this.place
-}
-Event_DataType.prototype.getType = function () {
-  return this.type  
-}
-Event_DataType.prototype.getUnit = function () {
-  return this.unit
+class Event_DataType {
+  constructor(time, place, type, unit) {
+    this.place = place;
+    this.time = time;
+    this.type = type;
+    this.unit = unit;
+  }
+  getTime() {
+    return this.time;
+  }
+  getPlace() {
+    return this.place;
+  }
+  getType() {
+    return this.type;
+  }
+  getUnit() {
+    return this.unit;
+  }
 }
 
 /*############# WeatherData Class ############*/
 
-let WeatherData = function (time, place, type, unit, value) {
-  Event_DataType.call(this, time, place, type, unit)
-  this.value = value
+class WeatherData extends Event_DataType{
+  constructor(time, place, type, unit, value) {
+    super(time, place, type, unit);
+    this.value = value;
+  }
+  getValue() {
+    return this.value;
+  }
 }
-WeatherData.prototype.getValue = function () {
-  return this.value
-}
-Object.setPrototypeOf(WeatherData.prototype, Event_DataType.prototype);
-
 
 /*###########Temperature Class#########*/
 
-let Temperature = function(time, place, type, unit, value) {
-  WeatherData.call(this, time, place, type, unit, value)
-}
-Temperature.prototype.convertToF = function() {
-  if (this.unit === 'C') {
-    this.unit = 'F'
-    this.value = this.value * 1.8 + 32
+class Temperature extends WeatherData{
+  constructor(time, place, type, unit, value) {
+    super(time, place, type, unit, value);
+  }
+  convertToF() {
+    if (this.unit === 'C') {
+      this.unit = 'F';
+      this.value = this.value * 1.8 + 32;
+    }
+  }
+  convertToC() {
+    if (this.unit === 'F') {
+      this.unit = 'C';
+      this.value = (this.value - 32) / 1.8;
+    }
   }
 }
-Temperature.prototype.convertToC = function() {
-  if (this.unit === 'F') {
-    this.unit = 'C'
-    this.value = (this.value - 32) / 1.8
-  }
-}
-Object.setPrototypeOf(Temperature.prototype, WeatherData.prototype);
 
 /*##########Precipitation Class########*/
 
-let Precipetation = function(time, place, type, unit, value){
-  WeatherData.call(this,time, place, type, unit, value)
-}
-Precipetation.prototype.getPrecipetationType = function(){
-  return WeatherData.getType();
-}
-Precipetation.prototype.convertToInches = function(){
-  if (this.unit === "MM") {
-    this.unit = "Inches"
-    this.value = this.value / (25.4)
+class Precipitation extends WeatherData{
+  constructor(time, place, type, unit, value) {
+   super(time, place, type, unit, value);
+  }
+  getPrecipitationType() {
+    return WeatherData.getType();
+  }
+  convertToInches() {
+    if (this.unit === "MM") {
+      this.unit = "Inches";
+      this.value = this.value / (25.4);
+    }
+  }
+  convertToMM() {
+    if (this.unit === "Inches") {
+      this.unit = "MM";
+      this.value = this.value * 25.4;
+    }
   }
 }
-Precipetation.prototype.convertToMM = function(){
-  if (this.unit === "Inches") {
-    this.unit = "MM"
-    this.value = this.value * 25.4
-  }
-}
-Object.setPrototypeOf(Precipetation.prototype, WeatherData.prototype)
 /*##############Wind Class#############*/
 
-let Wind = function(time, place, type, unit, value, direction){
-  WeatherData.call(this,time, place, type, unit, value)
-  this.direction = direction
-}
-Wind.prototype.getDirection = function(){
-  return this.direction
-}
-Wind.prototype.convertToMPH = function(){
-  if (this.unit === "MS") {
-    this.unit = "MPH"
-    this.value = this.value*2.237
+class Wind extends WeatherData{
+  constructor(time, place, type, unit, value, direction) {
+    super(time, place, type, unit, value);
+    this.direction = direction;
+  }
+  getDirection() {
+    return this.direction;
+  }
+  convertToMPH() {
+    if (this.unit === "MS") {
+      this.unit = "MPH";
+      this.value = this.value * 2.237;
+    }
+  }
+  convertToMS() {
+    if (this.unit === "MPH") {
+      this.unit = "MS";
+      this.value = this.value / 2.237;
+    }
   }
 }
-Wind.prototype.convertToMS = function(){
-  if (this.unit === "MPH") {
-    this.unit = "MS"
-    this.value = this.value/2.237
-  }
-}
-Object.setPrototypeOf(Wind.prototype, WeatherData.prototype)
-
 
 /*#########CloudCoverage Class#########*
 
 /* this class is originally empty so I got creative :))*/
-let CloudCoverage = function(time, place, type, unit, value, sky){
-  WeatherData.call(this,time, place, type, unit, value)
-  this.sky = sky
-}
-CloudCoverage.prototype.getSkyStatus = function(){
-  switch (this.value) {
-  case 0:
-    this.sky = "the sky is empty";
-      break;
-  case 1:
-    this.sky = "Clear";
-      break;
-  case 2:
-    this.sky = "Clear";
-      break;
-  case 3:
-    this.sky = "Kinda cloudy";
-      break;
-  case 4:
-    this.sky = "Half Cloudy";
-      break;
-  case 5:
-    this.sky = "Half Cloudy";
-      break;
-  case 6:
-    this.sky = "very Cloudy";
-      break;
-  case 7:
-    this.sky = "very Cloudy";
-      break;
-  case 8:
-    this.sky = "Completely Cloudy";
-      break;
-  case 9:
-    this.sky = "obstructed from view"
+class CloudCoverage extends WeatherData{
+  constructor(time, place, type, unit, value, sky) {
+    super(time, place, type, unit, value);
+    this.sky = sky;
   }
-  return "the sky is " + this.sky
+  getSkyStatus() {
+    switch (this.value) {
+      case 0:
+        this.sky = "the sky is empty";
+        break;
+      case 1:
+        this.sky = "Clear";
+        break;
+      case 2:
+        this.sky = "Clear";
+        break;
+      case 3:
+        this.sky = "Kinda cloudy";
+        break;
+      case 4:
+        this.sky = "Half Cloudy";
+        break;
+      case 5:
+        this.sky = "Half Cloudy";
+        break;
+      case 6:
+        this.sky = "very Cloudy";
+        break;
+      case 7:
+        this.sky = "very Cloudy";
+        break;
+      case 8:
+        this.sky = "Completely Cloudy";
+        break;
+      case 9:
+        this.sky = "obstructed from view";
+    }
+    return "the sky is " + this.sky;
+  }
 }
-Object.setPrototypeOf(CloudCoverage.prototype, WeatherData.prototype)
 
 /*##########DateInterval Class#########*/
 
-let DateInterval = function name(from, to) {
-  this.from = from;
-  this.to = to;
-}
-DateInterval.prototype.getFrom = function () {
-  this.from = from
-}
-DateInterval.prototype.getTo = function (){
-  this.to = to
-}
-DateInterval.prototype.contains = function (date){
-  if(date >= from && date <= to){
-    return true
-  }else{
-    return false
+class DateInterval {
+  constructor(from, to) {
+    this.from = from;
+    this.to = to;
+  }
+  getFrom() {
+    return this.from;
+  }
+  getTo() {
+    return this.to;
+  }
+  contains(date) {
+    if (date >= this.from && date <= this.to) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
 /*#########WeatherHistory Class########*/
-let WeatherHistory = function(data){
-  this.data = data
-  data = []
+class WeatherHistory {
+  constructor(data, placeFilter, typeFilter, periodFilter) {
+    this.data = data;
+    this.placeFilter = placeFilter;
+    this.typeFilter = typeFilter;
+    this.periodFilter = periodFilter;
+  }
+  getData(){
+    return this.data
+  }
+  setPlaceFilter(arg) {
+    this.placeFilter = arg;
+  }
+  getPlaceFilter() {
+    return this.placeFilter;
+  }
+  clearPlaceFilter() {
+    this.placeFilter = "";
+  }
+  setTypeFilter(arg) {
+    this.typeFilter = arg;
+  }
+  getTypeFilter() {
+    return this.typeFilter;
+  }
+  clearTypeFilter() {
+    this.periodFilter = "";
+  }
+  setPeriodFilter(arg) {
+    this.periodFilter = arg;
+  }
+  getPeriodFilter() {
+    return this.periodFilter;
+  }
+  clearPeriodFilter() {
+    this.periodFilter = "";
+  }
+  convertToUSUnits() {
+    data.forEach(item => {
+      if (item.getType() === 'Wind') {
+        item.convertToMPH();
+      }
+      else if (item.getType() === 'Temperature') {
+        item.convertToF();
+      }
+      else if (item.getType() === 'Precipitation') {
+        item.convertToInches();
+      }
+    });
+  }
+  convertToInternationalUnit() {
+    data.forEach((item) => {
+      if (item.getType() === 'Wind') {
+        item.convertToMS();
+      }
+      else if (item.getType() === 'Temperature') {
+        item.convertToC();
+      }
+      else if (item.getType() === 'Precipitation') {
+        item.convertToMM();
+      }
+    });
+  }
+  addNew(newData) {
+    this.data.push(newData);
+  }
+  getFilteredData() {
+    this.data.filter(item => item.getPlace() === this.placeFilter || item.getType() === this.typeFilter || this.getPeriodFilter().contains(item.time))
+  }
 }
 
 /*########tester#######*/
- let cloude1 = new CloudCoverage("soemthing", "Horsens","CloudeCoverage", "Okta",9,"")
- console.log(cloude1.getSkyStatus());
+//WEATHER HISTORY TESTING
+const fromTester = new Date('December 17, 1995 03:24:00')
+const toTester = new Date('December 17, 2021 05:00:00')
+
+let inter = new DateInterval(fromTester, toTester)
+const currentTime = new Date('September 17, 2000 15:35:00')
+
+const data1 = new Wind('West',currentTime,'Horsens','Wind','MS', 3)
+const data2 = new Wind(direction='East', time=currentTime, place= 'Aarhus', type='Wind', unit='MPH', value=15)
+
+const data3 = new Temperature("", place= 'Horsens', type='Temperature', unit='C', value=30)
+const data4 = new Temperature( time=currentTime, place= 'Aarhus', type='Temperature', unit='F', value=69)
+
+const data5 = new Precipitation(time=currentTime, place= 'Horsens', type='Precipitation', unit='MM', value=30)
+const data6 = new Precipitation(time=currentTime, place= 'Aarhus', type='Precipitation', unit='Inch', value=3)
+
+dataList = [data1, data2, data3, data4, data5, data6]
+// console.log(dataList)
+//console.log(dataList)
+const history = new WeatherHistory(dataList,"","","")
+
+//history.setTypeFilter('Temperature')
+//history.setPeriodFilter(inter)
+// console.log(history)
+//history.addNew(dataList)
+history.setPlaceFilter('Horsens')
+// console.log(history.getFilteredData())
+list = history.getFilteredData()
+console.log(list)
