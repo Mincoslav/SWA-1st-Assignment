3b4251da1e0f21730b5d91970b45539d907a09f1
Michal <mincoslav@gmail.com>
2021-11-08 15:58:07 +0100

    added user data submision through post method

--- exercise3.1/src/components/dataInput.js
+++ exercise3.1/src/components/dataInput.js
@@ -1,64 +1,5 @@
-// import $ from "jquery";
-// import axios from "axios";
-
-// export const SendHistoricalData = () => {
-// 	return (
-// 		<div>
-// 			<form>
-// 				<label htmlFor="type">Weather Type</label>
-// 				<br />
-				// <input id="type" list="types" />
-				// <datalist id="types">
-				// 	<option value="Temperature" />
-				// 	<option value="Wind" />
-				// 	<option value="Precipitation" />
-				// 	<option value="CloudCoverage" />
-				// </datalist>
-// 				<br />
-// 				<label htmlFor="value">Value</label>
-// 				<br />
-// 				<input type="text" id="value" name="value" />
-// 				<br />
-// 				<label htmlFor="place">City</label> <br />
-// 				<input list="place" />
-// 				<datalist id="place">
-// 					<option value="Horsens" />
-// 					<option value="Aarhus" />
-// 					<option value="Copenhagen" />
-// 				</datalist>
-// 				<br />
-// 				<label htmlFor="time">Time of reading (date and time):</label>
-// 				<br />
-// 				<input type="datetime-local" id="time" name="time" />
-// 				<br />
-// 				<button value="Send historical data" onClick={sendData()}>
-// 					Send historical data
-// 				</button>
-// 			</form>
-// 		</div>
-// 	);
-// };
-
-// const sendData = () => {
-// 	//package data into json and send it off.
-
-// 	let type = $("#type").val();
-// 	let value = $("#value").val();
-// 	let place = $("#place").val();
-// 	let time = $("#time").val();
-// 	// console.log(type, value, place, time);
-//     let weatherEvent = {
-//         value: value,
-//         type: type,
-//         time: time,
-//         place: place
-//     }
-//     console.log(weatherEvent)
-//     axios.post('http://localhost:8080/data/', weatherEvent)
-// };
-
+import axios from "axios";
 import { useState } from "react";
-
 
 export function DataSubmitForm() {
 	const [inputs, setInputs] = useState({});
@@ -66,47 +7,97 @@ export function DataSubmitForm() {
 	const handleChange = (event) => {
 		const name = event.target.name;
 		const value = event.target.value;
+		console.log(name);
+		console.log(value);
 		setInputs((values) => ({ ...values, [name]: value }));
 	};
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		alert(inputs);
-		console.log(inputs)
-		
+		// alert(inputs);
+		console.log(inputs);
+		//should add a check for wind and rain
+		axios
+			.post("http://localhost:8080/data", {
+				value: inputs.value,
+				type: inputs.type,
+				unit: inputs.unit,
+				time: inputs.time,
+				place: inputs.place
+			})
+			.then(function (response) {
+				alert(response.statusText)
+				console.log(response);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
 	};
 
 	return (
 		<form onSubmit={handleSubmit}>
-				<br/>
-				<label>
-					Choose weather type:
-					<br />
-					<select value={inputs.type} onChange={handleChange}>
-						<option value="temperature">Temperature</option>
-						<option value="wind">Wind</option>
-						<option value="cloudCoverage">Cloud Coverage</option>
-						<option value="precipitation">Precipitation</option>
-					</select>
-				</label>
-				<br />
-				<label>
-					<select value={inputs.city} onChange={handleChange}>
-						<option value="Horsens">Horsens</option>
-						<option value="Aarhus">Aarhus</option>
-						<option value="Copenhagen">Copenhagen</option>
-					</select>
-				</label>
-			
+			<br />
 			<label>
-				Enter your age:
+				Choose weather type:
+				<br />
+				<select name="type" value={inputs.type} onChange={handleChange}>
+					<option value=""></option>
+					<option value="temperature">Temperature</option>
+					<option value="wind">Wind</option>
+					<option value="cloudCoverage">Cloud Coverage</option>
+					<option value="precipitation">Precipitation</option>
+				</select>
+			</label>
+			<br />
+			<label>
+				Choose city:
+				<br />
+				<select name="place" value={inputs.city} onChange={handleChange}>
+					<option value=""></option>
+					<option value="Horsens">Horsens</option>
+					<option value="Aarhus">Aarhus</option>
+					<option value="Copenhagen">Copenhagen</option>
+				</select>
+			</label>
+			<br />
+			<label>
+				Enter value:
+				<br />
 				<input
 					type="number"
-					name="age"
-					value={inputs.age || ""}
+					name="value"
+					value={inputs.value || ""}
 					onChange={handleChange}
 				/>
 			</label>
+			<br />
+			<label>
+				Choose unit:
+				<br />
+				<select name="unit" value={inputs.unit} onChange={handleChange}>
+					<option value=""></option>
+					<option value="C">Degrees Celsius</option>
+					<option value="F">Degrees Fahrenheit</option>
+					<option value="m/s">Meters per second</option>
+					<option value="mph">Miles per hour</option>
+					<option value="mm">Millimeters</option>
+					<option value="inch">Inches</option>
+					<option value="%">Cloud percentage</option>
+				</select>
+			</label>
+			<br />
+			<label>
+				Choose date & time:
+				<br />
+				<input
+					name="time"
+					type="datetime-local"
+					value={inputs.time}
+					onChange={handleChange}
+				/>
+			</label>
+			<br />
+			<br />
 			<input type="submit" />
 		</form>
 	);
